aaaceb020962e1ab4a4b7d62bd7b1772f2d6b31c
Enrico De Michele <kennymuse@gmail.com>
2016-08-29 18:21:26 +0200

    Initial Commit

---
+++ UIFloatingButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIFloatingButton
+//
+//  Created by Enrico De Michele on 29/08/16.
+//  Copyright © 2016 Enrico De Michele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIFloatingButton/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UIFloatingButton
+//
+//  Created by Enrico De Michele on 29/08/16.
+//  Copyright © 2016 Enrico De Michele. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
